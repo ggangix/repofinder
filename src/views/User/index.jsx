4f087a20db5f955954634cd103e5438bbac217aa
Giuseppe <ggangix@gmail.com>
2021-12-18 15:08:36 +0100

    Add input to search

--- src/views/User/index.jsx
+++ src/views/User/index.jsx
@@ -1,4 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+  useMemo,
+} from "react";
 import { useParams } from "react-router-dom";
 import { getRepos, getUser } from "../../services/api";
 import UserContext from "../../store/userContext";
@@ -9,6 +15,7 @@ import "./style.scss";
 function User() {
   const { userData, setUserData } = useContext(UserContext);
   const [userRepos, setUserRepos] = useState([]);
+  const [search, setSearch] = useState("");
   const { username } = useParams();
 
   useEffect(() => {
@@ -20,6 +27,16 @@ function User() {
     if (username) getAllRepos(username);
   }, [setUserData, userData.name, username]);
 
+  const filteredRepos = useMemo(() => {
+    return userRepos.filter((repo) => {
+      return repo.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [userRepos, search]);
+
+  const searchRepos = useCallback((e) => {
+    setSearch(e.target.value);
+  }, []);
+
   const getAllRepos = async (username) => {
     const userRepos = await getRepos(username);
     setUserRepos(userRepos);
@@ -27,8 +44,15 @@ function User() {
 
   return (
     <div className="user">
-      {userData.name && <UserInfoWrapper userData={userData} />}
-      {userRepos.length > 0 && <UserReposWrapper userRepos={userRepos} />}
+      <input
+        type="text"
+        onChange={searchRepos}
+        placeholder="Search for a repository"
+      />
+      <div className="userData">
+        {userData.name && <UserInfoWrapper userData={userData} />}
+        {userRepos.length > 0 && <UserReposWrapper userRepos={filteredRepos} />}
+      </div>
     </div>
   );
 }
