453223eb92c9d4d4e175b333f628267f13ff6b1a
Giuseppe <ggangix@gmail.com>
2021-12-18 13:00:51 +0100

    remove unused

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../views/Home";
 import User from "../views/User";
 import NotFound from "../views/NotFound";
