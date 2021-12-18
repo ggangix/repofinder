33e624ca3e596b309a8aedac9d53bd0d027fc256
Giuseppe <ggangix@gmail.com>
2021-12-18 15:42:36 +0100

    Get more results from repos

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,9 @@ const API_URL = 'https://api.github.com/users/';
 
 module.exports = {
     getRepos: function(username) {
-        return fetch(API_URL + username + '/repos');
+      return fetch(`${API_URL + username}/repos?per_page=100`).then(res => res.json());
     },
     getUser: function(username) {
-        return fetch(API_URL + username);
+      return fetch(API_URL + username).then(res => res.json());
     }
 } 
