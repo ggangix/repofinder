90bfb9458f4261735dcc68171087257b4431ca4f
Giuseppe <ggangix@gmail.com>
2021-12-18 13:00:17 +0100

    Fix logo src

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header">
       <div className="logo">
-        <img src="./logo.png" alt="logo" />
+        <img src="/logo.png" alt="logo" />
       </div>
     </div>
   );
